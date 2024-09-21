570a290ffc7a2903c9778173175cc9b22341e059
Brian LewConklin <brianjl1944@gmail.com>
2024-09-21 13:34:39 -0400

    first project date class and class notes

---
+++ classwork/date_20_9_24/NewtonsMethod.java
@@ -0,0 +1,13 @@
+package classwork.date_20_9_24;
+
+public class NewtonsMethod {
+    
+    public static double sqrt(double a) {
+        double x = a + 1;
+        while (x * x - a > 1e-10) {
+            x = x - (x * x - a) / (2.0 * x);
+        }
+        return x;
+    }
+
+}
