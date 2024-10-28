dfa10544cd74ca8c62c531075e30265a34eb0217
Brian LewConklin <brianjl1944@gmail.com>
2024-10-27 19:22:32 -0700

    workin on project_3

---
+++ challenges/InterfaceCC/HourlyEmployee.java
@@ -0,0 +1,34 @@
+package challenges.InterfaceCC;
+
+public class HourlyEmployee extends Employee {
+    
+    private double hoursWorked;
+
+    private double hourlyRate;
+
+    public HourlyEmployee(String name){
+        super(name, 0.0);
+    }
+
+    public void setHoursWorked(double hoursWorked){
+        this.hoursWorked = hoursWorked;
+    }
+
+    public void setHourlyRate(double hourlyRate){
+        this.hourlyRate = hourlyRate;
+    }
+
+    public double getHoursWorked(){
+        return this.hoursWorked;
+    }
+
+    public double getHourlyRate(){
+        return this.hourlyRate;
+    }
+
+    @Override
+    public double getSalary(){
+        return getHoursWorked() * getHourlyRate();
+    }
+
+}
