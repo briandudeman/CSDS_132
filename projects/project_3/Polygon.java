bc951d1439583580d375f1836ff53a064aa742a4
Brian LewConklin <brianjl1944@gmail.com>
2024-10-27 22:25:42 -0400

    polygon work

--- projects/project_3/Polygon.java
+++ projects/project_3/Polygon.java
@@ -2,8 +2,14 @@ package projects.project_3;
 
 public abstract class Polygon{
 
-    public Polygon(Point[] points){
+    public Points[] points;
 
+    public Polygon(Point[] points){
+        this.points = points;
+    }
+
+    public Point getCenter() {
+        
     }
     
 }
