aaffdb12899668e22e27895b9637a46f85856606
Brian LewConklin <brianjl1944@gmail.com>
2024-11-03 16:30:41 -0800

    Revert "moved a bunch of stuff around"
    This reverts commit df044c455efac665e14166905113f2bf55f12be1.

---
+++ classwork/date_30_10_24/LinkedListDemo.java
@@ -0,0 +1,63 @@
+package classwork.date_28_10_24;
+
+import java.util.NoSuchElementException;
+
+import classwork.date_28_10_24.LLNode;
+
+public class LinkedListDemo<T> {
+
+    private LLNode<T> firstNode;
+
+    public LinkedListDemo() {
+        this.setFirstNode(null);
+    }
+
+    public boolean isEmpty() {
+        return this.getFirstNode() == null;
+    }
+
+    public void addToFront(T element) {
+        this.setFirstNode(new LLNode<T> (element, this.getFirstNode()));
+    }
+
+    protected LLNode<T> getFirstNode() {
+        return this.firstNode;
+    }
+
+    protected void setFirstNode(LLNode<T> firstNode) {
+        this.firstNode = firstNode;
+    }
+
+    public T removeFromFront() {
+        if (!this.isEmpty()) {
+            T save = this.getFirstNode().getElement();
+            this.setFirstNode(this.getFirstNode().getNext());
+            return save;
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    public int length() {
+        int count = 0;
+        LLNode<T> nodeptr = this.getFirstNode();
+        while (nodeptr != null) {
+            count++;
+            nodeptr = nodeptr.getNext();
+        }
+        return count;
+    }
+
+    public void addToEnd(T element) {
+        if (!isEmpty()) {
+            LLNode<T> nodeptr = this.getFirstNode();
+            while (nodeptr.getNext() != null) {
+                nodeptr = nodeptr.getNext();
+            }
+            nodeptr.setNext(new LLNode<T> (element, null));
+        } else {
+            this.addToFront(element);
+        }
+    }
+}
+
--- test1_foler/test1.java
+++
@@ -1,7 +0,0 @@
-public class test1 {
-
-    public static void test1_print() {
-        System.out.println("thing");
-    }
-    
-}
