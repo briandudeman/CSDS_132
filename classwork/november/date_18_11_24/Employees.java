9985a4c87cce47ec8e17a89d51aa662bab34416f
Brian LewConklin <brianjl1944@gmail.com>
2024-11-18 15:21:57 -0500

    daet_18_11_24

--- classwork/november/date_18_11_24/Employees.java
+++ classwork/november/date_18_11_24/Employees.java
@@ -1,11 +1,22 @@
 package date_18_11_24;
 
 import date_8_11_24.Employee;
-import october.date_30_10_24.LinkedListDemo;
 
 public class Employees {
     
-    public static void giveRaise(LinkedListDemo<Employee> employees, double raise) {
-        employees.iterator().forEachRemaining(e -> esetSalary(e.getSalary() * (1 + raise)));
+    public static void giveRaise(LinkedList<Employee> employees, double raise) {
+        employees.iterator().forEachRemaining(e -> e.setSalary(e.getSalary() * (1 + raise)));
     }
+    
+    public static void setMinWage(LinkedList<HourlyEmployee> employees, double wage) {
+        employees.iterator().forEachRemaining(e -> {if (e.getHourlyRate() < wage) e.setHourlyRate(wage); });
+    }
+
+    public static void giveBonus(LinkedList<HourlyEmployee> employees, double wage, double raise) {
+        Consumer<HourlyEmployee> setMinWage = e -> { if (e.getHourlyRate() < wage) e.setHourlyRate(wage); };
+        Consumer<HourlyEmployee> giveRaise = e -> e.setHourlyRate(e.getHourlyRate() * (1 + raise));
+    
+        employees.iterator().forEachRemaining(setMinWage.andThen(giveRaise));
+    }
+
 }
--- classwork/november/date_18_11_24/notes.md
+++ classwork/november/date_18_11_24/notes.md
@@ -1,2 +1,7 @@
 ## lambda shortcuts
-1. java 8 added teh lambda shortcut for anonymous classes that implement interfaces with a single non-default method and updated many classes in the API to expect to use them
+1. java 8 added the lambda shortcut for anonymous classes that implement interfaces with a single non-default method and updated many classes in the API to expect to use them
+
+
+1. monad: a srappwer class that has a context
+   1. the context chnages based on the value it is wrapping
+   2. Optional: a monda class and the context is whether or not the wrapped balue is null
